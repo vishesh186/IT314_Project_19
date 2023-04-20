189b1dfa7435d2f36356823d09b4a48c994bf470
Vishesh Patel <124247859+vishesh186@users.noreply.github.com>
2023-04-20 13:47:25 +0530

    Employee Model
    Employee Model created using Djongo

--- ProjectManagementSystemApp/models.py
+++ ProjectManagementSystemApp/models.py
@@ -1,3 +1,48 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+class Employee(models.Model):
+    employeeID = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=64)
+    email = models.EmailField()
+    joiningDate = models.DateField(null=True, blank=True)
+    salary = models.PositiveIntegerField(default=0)
+    role = models.CharField(max_length=6, default='E',
+        choices=[
+            ("PM", "Project Manager"),
+            ("RM", "Resource Manager"),
+            ("E", "Employee"),
+        ])
+
+
+    def __str__(self):
+        return self.employeeID
+    
+
+class EmployeeForm(ModelForm):
+    class Meta:
+        model = Employee
+        fields = ['employeeID', 'name', 'email', 'role']
+
+
+@receiver(post_save, sender=User)
+def UpdateEmployee(sender, instance=None, created=False, **kwargs):
+    if created:
+        Employee.objects.create(
+            employeeID=instance.username,
+            name=instance.get_full_name(),
+            email=instance.email
+        )
+    else:
+        try:
+            employee = Employee.objects.get(employeeID=instance.username)
+            employee.employeeID = instance.username
+            employee.name = instance.get_full_name()
+            employee.email = instance.email
+            employee.save()
+        except:
+            Employee.objects.create(
+                employeeID=instance.username,
+                name=instance.get_full_name(),
+                email=instance.email
+            )
