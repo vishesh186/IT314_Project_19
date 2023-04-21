2d98b2424c24b3993c96dcd8cbddfa8a9853b771
Vishesh Patel <124247859+vishesh186@users.noreply.github.com>
2023-04-22 00:51:40 +0530

    Admin Setup

--- ProjectManagementSystemApp/admin.py
+++ ProjectManagementSystemApp/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import *
+from django_json_widget.widgets import JSONEditorWidget
+from django import forms
+
+# Register your models here.
+
+class ProjectForm(forms.ModelForm):
+    team = forms.JSONField()
+    class Meta:
+        model = Project
+        fields = "__all__"
+
+class ProjectAdmin(admin.ModelAdmin):
+    form = ProjectForm
+
+admin.site.register(Project, ProjectAdmin)
+
+
+class EmployeeForm(forms.ModelForm):
+    team = forms.JSONField()
+    class Meta:
+        model = Employee
+        fields = "__all__"
+
+class EmployeeAdmin(admin.ModelAdmin):
+    form = EmployeeForm
+
+admin.site.register(Employee, EmployeeAdmin)
+
+
+admin.site.register(Team)
+
+admin.site.register(Task)
